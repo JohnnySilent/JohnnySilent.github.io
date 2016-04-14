589a265a6ee736fa3a4a1d27353b8983b4c1a2ba
JohnnySilent <johnny0silent@gmail.com>
2016-04-14 15:15:44 -0500

    MORE CORRECTIONS

---
+++ practice.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php
+		$Name = "Ryhme";
+		$num = 23;
+
+		echo "you hear a name in your head: " . $Name . ". It sound famialr to you...";
+
+	?>
+</body>
+</html>
